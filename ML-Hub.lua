6bb70e969ad354b917bd97b6977743ce6178ff60
creator-26 <pholalexanderwuamputsaggamboa@gmail.com>
2025-11-07 14:48:41 -0500

    Change button height in ML-Hub.lua
    Adjusted button height from 35 to 32.

--- ML-Hub.lua
+++ ML-Hub.lua
@@ -43,7 +43,7 @@ local btn = VisualHub:AddButton(gui, "Anti AFK", function()
         end
     end)
 end, y)
-btn.Size = UDim2.new(0, 150, 0, 35)  -- ancho 150, alto 35 (ajusta como prefieras)
+btn.Size = UDim2.new(0, 150, 0, 32)  -- ancho 150, alto 35 (ajusta como prefieras)
 btn.Position = UDim2.new(0, 15, 0, y) -- opcional, para mantener alineado
 y = y + 30
 
