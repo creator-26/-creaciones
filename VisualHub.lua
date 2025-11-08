af7d81c4c289561c7dc7a935760366f2eee84a03
creator-26 <pholalexanderwuamputsaggamboa@gmail.com>
2025-11-08 08:26:34 -0500

    Update VisualHub.lua

--- VisualHub.lua
+++ VisualHub.lua
@@ -93,7 +93,7 @@ end
 function VisualHub:AddSwitch(frame, lbltext, callback, ypos)
     local anchoBack = 340   -- Ancho total del label azul/gris, lo suficiente para cubrir casi todo el menú
     local anchoSw = 52     -- Ancho del botón ON/OFF (igual o apenas menor que el de la imagen)
-    local alturaLabel = 34  -- Ajusta para igualar visualmente tu diseño (alto del label)
+    local alturaLabel = 30  -- Ajusta para igualar visualmente tu diseño (alto del label)
     local alturaSwitch = 15 -- Más bajo que el label para que quede en el centro
     local ajuste = math.floor((alturaLabel - alturaSwitch)/2)
     -- Label grande de color (fondo)
